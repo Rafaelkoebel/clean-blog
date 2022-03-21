183928baa9109c07d735def5b0cf9c42319b83d2
Rafaelkoebel <rafael.koebel@gmail.com>
2022-03-21 16:51:00 +0100

    PDO

--- model/home.model.php
+++ model/home.model.php
@@ -1,4 +1,6 @@
 <?php 
+    include("config/config.inc.php");
+    include("model/pdo.inc.php");
     $bg="assets/img/home-bg.jpg";
     $title="Blog de surf";
     $subtitle="Comment bien lui demander";
